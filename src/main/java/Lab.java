e67e7d570b841c37f2aace97b02c961ef45ed5c7
sushmishra87 <sm.sushmimishra@gmail.com>
2023-03-01 03:47:17 +0000

    Completed Lab ListExerciseTest

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,4 @@
-
-import java.util.List;
+import java.util.*;
 
 /**
  * Lists are ordered collections. This means that we could access elements at a certain position of a list,
@@ -28,8 +27,10 @@ public class Lab {
      *
      * @return a List<Integer> object.
      */
-    public List<Integer> createList(){
-        return null;
+    public List<Integer> createList()
+    {
+    	List<Integer> list = new ArrayList<Integer>();
+    	return list;
     }
 
     /**
@@ -38,8 +39,11 @@ public class Lab {
      * @param list a List<Integer> object.
      * @return the size of List (number of items it holds.)
      */
-    public int getSize(List<Integer> list){
-        return 0;
+    public int getSize(List<Integer> list)
+    {
+    	//list = new ArrayList<>();
+    	int size = list.size();
+        return size;
     }
 
     /**
@@ -50,7 +54,9 @@ public class Lab {
      * @param value an integer that we would like to add to list.
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void addToList(List<Integer> list, int value){
+    public void addToList(List<Integer> list, int value)
+    {
+    	list.add(value);	
     }
 
     /**
@@ -65,8 +71,9 @@ public class Lab {
      * @param index represents the index of the element we would like to retrieve.
      * @return the int at the location in 'list' represented by 'index'.
      */
-    public int get(List<Integer> list, int index){
-        return 0;
+    public int get(List<Integer> list, int index)
+    {
+        return list.get(index);
     }
 
     /**
@@ -78,8 +85,9 @@ public class Lab {
      * @param position represents the index of the element we would like to remove.
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void removeFromList(List<Integer> list, int position){
-
+    public void removeFromList(List<Integer> list, int position)
+    {
+    	list.remove(position);
     }
 
     /**
@@ -91,6 +99,8 @@ public class Lab {
      * @param value the new value which we would like to assign to the item at position in list
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void updateAtPosition(List<Integer> list, int position, int value){
+    public void updateAtPosition(List<Integer> list, int position, int value)
+    {
+    	list.set(position, value);
     }
 }
